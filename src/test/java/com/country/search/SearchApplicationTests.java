6a001d9863b2c37fe247e7fbb2a88e4212c1c1a3
Dmitrii Lavlinskii <Dmitrii.Lavlinskii@gmail.com>
2021-04-03 10:46:08 -0500

    Added empty Spring Boot project.

---
+++ src/test/java/com/country/search/SearchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.country.search;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SearchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
